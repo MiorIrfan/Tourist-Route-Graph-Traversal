81f2f10470c7a7eb7c32ce14737c094b7fb241fb
NazmiHashim <nazmih79@gmail.com>
2023-07-08 22:54:29 +0800

    Added total length and cities

--- src/ModifiedFinder.java
+++ src/ModifiedFinder.java
@@ -1,13 +1,13 @@
 import java.util.*;
 
-public class PathFinderDFS {
+public class ModifiedFinder {
     private int numVertices;
     private List<Integer>[] adjacencyList;
     private List<Edge>[] edgeList;
     private List<Integer> path;
     private boolean pathFound;
 
-    public PathFinderDFS(int numVertices) {
+    public ModifiedFinder(int numVertices) {
         this.numVertices = numVertices;
         adjacencyList = new ArrayList[numVertices];
         edgeList = new ArrayList[numVertices];
@@ -83,34 +83,37 @@ public class PathFinderDFS {
     }
 
     public static void main(String[] args) {
-        // Create a transportation system graph
-        int numVertices = 7;
-        PathFinderDFS pathFinder = new PathFinderDFS(numVertices);
-        pathFinder.addEdge(0, 1, 5);  // Vertex 0 (source) to Vertex 1 with value 5
-        pathFinder.addEdge(0, 2, 7);  // Vertex 0 (source) to Vertex 2 with value 7
-        pathFinder.addEdge(1, 3, 10); // Vertex 1 to Vertex 3 with value 10
-        pathFinder.addEdge(2, 3, 3);  // Vertex 2 to Vertex 3 with value 3
-        pathFinder.addEdge(3, 4, 8);  // Vertex 3 to Vertex 4 with value 8
-        pathFinder.addEdge(3, 5, 2);  // Vertex 3 to Vertex 5 with value 2
-        pathFinder.addEdge(4, 5, 6);  // Vertex 4 to Vertex 6 with value 6
-        pathFinder.addEdge(5, 1, 4);  // Vertex 5 to Vertex 6 with value 4
+        // Create a road network graph
+        int numCities = 7;
+        ModifiedFinder pathFinder = new ModifiedFinder(numCities);
+        pathFinder.addEdge(0, 1, 5);  // Road from City 0 (source) to City 1 with distance 5
+        pathFinder.addEdge(0, 2, 7);  // Road from City 0 (source) to City 2 with distance 7
+        pathFinder.addEdge(1, 3, 10); // Road from City 1 to City 3 with distance 10
+        pathFinder.addEdge(2, 3, 3);  // Road from City 2 to City 3 with distance 3
+        pathFinder.addEdge(3, 4, 8);  // Road from City 3 to City 4 with distance 8
+        pathFinder.addEdge(3, 5, 2);  // Road from City 3 to City 5 with distance 2
+        pathFinder.addEdge(4, 5, 6);  // Road from City 4 to City 6 with distance 6
+        pathFinder.addEdge(5, 1, 4);  // Road from City 5 to City 6 with distance 4
 
-        // Prompt the user to enter the source and destination vertices
+        // Prompt the user to enter the source and destination cities
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter source vertex: ");
-        int source = scanner.nextInt();
-        System.out.print("Enter destination vertex: ");
-        int destination = scanner.nextInt();
+        System.out.print("Enter source city: ");
+        String sourceCity = scanner.nextLine();
+        System.out.print("Enter destination city: ");
+        String destinationCity = scanner.nextLine();
 
         // Find the path using DFS
+        int source = cityToVertex(sourceCity);
+        int destination = cityToVertex(destinationCity);
         List<Integer> path = pathFinder.findPathDFS(source, destination);
 
         // Output the result
         if (path != null) {
-            System.out.println("Path from " + source + " to " + destination + ":");
+            System.out.println("Path from " + sourceCity + " to " + destinationCity + ":");
             for (int i = 0; i < path.size(); i++) {
                 int vertex = path.get(i);
-                System.out.print(vertex);
+                String city = vertexToCity(vertex);
+                System.out.print(city);
                 if (i != path.size() - 1) {
                     System.out.print(" -> ");
                 }
@@ -125,12 +128,53 @@ public class PathFinderDFS {
                 int edgeWeight = pathFinder.getEdgeValue(sourceVertex, destinationVertex);
                 totalWeight += edgeWeight;
             }
-
-            System.out.println("Total length of the path: " + totalWeight);
-
-
+            System.out.println("Total distance of the path: " + totalWeight + " km");
         } else {
-            System.out.println("No path from " + source + " to " + destination + " found.");
+            System.out.println("No path from " + sourceCity + " to " + destinationCity + " found.");
+        }
+    }
+
+    // Helper method to convert city name to vertex number
+    private static int cityToVertex(String city) {
+        switch (city) {
+            case "Butterworth":
+                return 0;
+            case "Jelutong":
+                return 1;
+            case "Air Itam":
+                return 2;
+            case "Gelugor":
+                return 3;
+            case "George Town ":
+                return 4;
+            case "Balik Pulau":
+                return 5;
+            case "Bayan Lepas":
+                return 6;
+            default:
+                throw new IllegalArgumentException("Invalid city name: " + city);
+        }
+    }
+
+    // Helper method to convert vertex number to city name
+    private static String vertexToCity(int vertex) {
+        switch (vertex) {
+            case 0:
+                return "Butterworth";
+            case 1:
+                return "Jelutong";
+            case 2:
+                return "Air Itam";
+            case 3:
+                return "Gelugor";
+            case 4:
+                return "George Town";
+            case 5:
+                return "Balik Pulau";
+            case 6:
+                return "Bayan Lepas";
+            default:
+                throw new IllegalArgumentException("Invalid vertex number: " + vertex);
         }
     }
 }
