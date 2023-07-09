5616c6cb34e05ed937948e5f0af3eb9364d243fe
Mior Irfan <miorirfan02@gmail.com>
2023-07-09 14:46:33 +0800

    Add some codes

--- src/ModifiedFinder.java
+++ src/ModifiedFinder.java
@@ -84,16 +84,76 @@ public class ModifiedFinder {
 
     public static void main(String[] args) {
         // Create a road network graph
-        int numCities = 7;
+        int numCities = 48;
         ModifiedFinder pathFinder = new ModifiedFinder(numCities);
-        pathFinder.addEdge(0, 1, 5);  // Road from City 0 (source) to City 1 with distance 5
-        pathFinder.addEdge(0, 2, 7);  // Road from City 0 (source) to City 2 with distance 7
-        pathFinder.addEdge(1, 3, 10); // Road from City 1 to City 3 with distance 10
-        pathFinder.addEdge(2, 3, 3);  // Road from City 2 to City 3 with distance 3
-        pathFinder.addEdge(3, 4, 8);  // Road from City 3 to City 4 with distance 8
-        pathFinder.addEdge(3, 5, 2);  // Road from City 3 to City 5 with distance 2
-        pathFinder.addEdge(4, 5, 6);  // Road from City 4 to City 6 with distance 6
-        pathFinder.addEdge(5, 1, 4);  // Road from City 5 to City 6 with distance 4
+        //502
+        pathFinder.addEdge(1, 2, 5);  // Road from City 0 (source) to City 1 with distance 5
+        pathFinder.addEdge(2, 3, 7);  // Road from City 0 (source) to City 2 with distance 7
+        pathFinder.addEdge(3, 4, 10); // Road from City 1 to City 3 with distance 10
+        pathFinder.addEdge(4, 5, 3);  // Road from City 2 to City 3 with distance 3
+        pathFinder.addEdge(5, 6, 8);  // Road from City 3 to City 4 with distance 8
+
+        //502 reverse
+        pathFinder.addEdge(6, 5, 5);  // Road from City 0 (source) to City 1 with distance 5
+        pathFinder.addEdge(5, 4, 7);  // Road from City 0 (source) to City 2 with distance 7
+        pathFinder.addEdge(4, 3, 10); // Road from City 1 to City 3 with distance 10
+        pathFinder.addEdge(3, 2, 3);  // Road from City 2 to City 3 with distance 3
+        pathFinder.addEdge(2, 1, 8);  // Road from City 3 to City 4 with distance 8
+
+        //102
+        pathFinder.addEdge(7, 8, 2);  // Road from City 3 to City 5 with distance 2
+        pathFinder.addEdge(8, 9, 6);  // Road from City 4 to City 6 with distance 6
+        pathFinder.addEdge(9, 4, 4);  // Road from City 5 to City 6 with distance 4
+        pathFinder.addEdge(4, 5, 5);  // Road from City 0 (source) to City 1 with distance 5
+        pathFinder.addEdge(5, 10, 7);  // Road from City 0 (source) to City 2 with distance 7
+        pathFinder.addEdge(10, 11, 10); // Road from City 1 to City 3 with distance 10
+        pathFinder.addEdge(11, 12, 3);  // Road from City 2 to City 3 with distance 3
+
+        //102 reverse
+        pathFinder.addEdge(12, 11, 2);  // Road from City 3 to City 5 with distance 2
+        pathFinder.addEdge(11, 10, 6);  // Road from City 4 to City 6 with distance 6
+        pathFinder.addEdge(10, 5, 4);  // Road from City 5 to City 6 with distance 4
+        pathFinder.addEdge(5, 4, 2);  // Road from City 3 to City 5 with distance 2
+        pathFinder.addEdge(4, 9, 6);  // Road from City 4 to City 6 with distance 6
+        pathFinder.addEdge(9, 8, 4);  // Road from City 5 to City 6 with distance 4
+        pathFinder.addEdge(8, 7, 2);  // Road from City 3 to City 5 with distance 2
+
+        //306
+        pathFinder.addEdge(7, 14, 8);  // Road from City 3 to City 4 with distance 8
+        pathFinder.addEdge(14, 19, 2);  // Road from City 3 to City 5 with distance 2
+        pathFinder.addEdge(19, 15, 6);  // Road from City 4 to City 6 with distance 6
+        pathFinder.addEdge(15, 16, 4);  // Road from City 5 to City 6 with distance 4
+        pathFinder.addEdge(16, 4, 8);  // Road from City 3 to City 4 with distance 8
+        pathFinder.addEdge(4, 10, 2);  // Road from City 3 to City 5 with distance 2
+
+        //306 reverse
+        pathFinder.addEdge(10, 4, 6);  // Road from City 4 to City 6 with distance 6
+        pathFinder.addEdge(4, 16, 4);  // Road from City 5 to City 6 with distance 4
+        pathFinder.addEdge(16, 15, 6);  // Road from City 4 to City 6 with distance 6
+        pathFinder.addEdge(15, 19, 4);  // Road from City 5 to City 6 with distance 4
+        pathFinder.addEdge(19, 14, 6);  // Road from City 4 to City 6 with distance 6
+        pathFinder.addEdge(14, 7, 4);  // Road from City 5 to City 6 with distance 4
+
+        //302
+        pathFinder.addEdge(13, 20, 8);  // Road from City 3 to City 4 with distance 8
+        pathFinder.addEdge(20, 14, 2);  // Road from City 3 to City 5 with distance 2
+        pathFinder.addEdge(14, 15, 6);  // Road from City 4 to City 6 with distance 6
+        pathFinder.addEdge(15, 17, 4);  // Road from City 5 to City 6 with distance 4
+        pathFinder.addEdge(17, 18, 8);  // Road from City 3 to City 4 with distance 8
+        pathFinder.addEdge(18, 6, 2);  // Road from City 3 to City 5 with distance 2
+
+        //302 reverse
+        pathFinder.addEdge(6, 18, 8);  // Road from City 3 to City 4 with distance 8
+        pathFinder.addEdge(18, 17, 2);  // Road from City 3 to City 5 with distance 2
+        pathFinder.addEdge(17, 15, 6);  // Road from City 4 to City 6 with distance 6
+        pathFinder.addEdge(15, 14, 4);  // Road from City 5 to City 6 with distance 4
+        pathFinder.addEdge(14, 20, 8);  // Road from City 3 to City 4 with distance 8
+        pathFinder.addEdge(20, 13, 2);  // Road from City 3 to City 5 with distance 2
+
+        //Display message to welcome user
+        System.out.print("******Welcome to Tourist Navigation Plans Application******\n");
+        System.out.print("You can see the available places to visit from your current location\n");
+        System.out.print("This application will suggest top 1 routes that you can take\n\n");
 
         // Prompt the user to enter the source and destination cities
         Scanner scanner = new Scanner(System.in);
@@ -101,6 +161,8 @@ public class ModifiedFinder {
         String sourceCity = scanner.nextLine();
         System.out.print("Enter destination city: ");
         String destinationCity = scanner.nextLine();
+
+        System.out.println();
 
         // Find the path using DFS
         int source = cityToVertex(sourceCity);
@@ -137,20 +199,46 @@ public class ModifiedFinder {
     // Helper method to convert city name to vertex number
     private static int cityToVertex(String city) {
         switch (city) {
-            case "Butterworth":
-                return 0;
-            case "Jelutong":
-                return 1;
-            case "Air Itam":
-                return 2;
-            case "Gelugor":
-                return 3;
-            case "George Town ":
-                return 4;
             case "Balik Pulau":
+                return 1;
+            case "Muzium Cafe Western":
+                return 2;
+            case "Kek Lok Si Temple":
+                return 3;
+            case "Masjid Negeri":
+                return 4;
+            case "Komtar":
                 return 5;
-            case "Bayan Lepas":
+            case "Jetty":
                 return 6;
+            case "Airport Penang":
+                return 7;
+            case "Sungai Tiram":
+                return 8;
+            case "Gelugor Penang":
+                return 9;
+            case "General Hospital Penang":
+                return 10;
+            case "Batu Ferringhi":
+                return 11;
+            case "Teluk Bahang":
+                return 12;
+            case "Batu Maung":
+                return 13;
+            case "Sunshine Bayan Baru":
+                return 14;
+            case "Kompleks Bukit Jambul":
+                return 15;
+            case "Oriental Garden":
+                return 16;
+            case "Jelutong":
+                return 17;
+            case "Sungai Pinang":
+                return 18;
+            case "Queensbay":
+                return 19;
+            case "Muzium Perang":
+                return 20;
             default:
                 throw new IllegalArgumentException("Invalid city name: " + city);
         }
@@ -159,20 +247,46 @@ public class ModifiedFinder {
     // Helper method to convert vertex number to city name
     private static String vertexToCity(int vertex) {
         switch (vertex) {
-            case 0:
-                return "Butterworth";
             case 1:
-                return "Jelutong";
-            case 2:
-                return "Air Itam";
-            case 3:
-                return "Gelugor";
-            case 4:
-                return "George Town";
-            case 5:
                 return "Balik Pulau";
+            case 2:
+                return "Muzium Cafe Western";
+            case 3:
+                return "Kek Lok Si Temple";
+            case 4:
+                return "Masjid Negeri";
+            case 5:
+                return "Komtar";
             case 6:
-                return "Bayan Lepas";
+                return "Jetty";
+            case 7:
+                return "Airport Penang";
+            case 8:
+                return "Sungai Tiram";
+            case 9:
+                return "Gelugor Penang";
+            case 10:
+                return "General Hospital Penang";
+            case 11:
+                return "Batu Ferringhi";
+            case 12:
+                return "Teluk Bahang";
+            case 13:
+                return "Batu Maung";
+            case 14:
+                return "Sunshine Bayan Baru";
+            case 15:
+                return "Kompleks Bukit Jambul";
+            case 16:
+                return "Oriental Garden";
+            case 17:
+                return "Jelutong";
+            case 18:
+                return "Sungai Pinang";
+            case 19:
+                return "Queensbay";
+            case 20:
+                return "Muzium Perang";
             default:
                 throw new IllegalArgumentException("Invalid vertex number: " + vertex);
         }
